dd245926e27f9ed0017a8c88c952dd9241c5ae13
justbarnt <functnal@gmail.com>
2025-03-07 16:42:04 -0500

    working on native lsp

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
-require("justbarnt")
+---@diagnostic disable-next-line: undefined-global
+if init_debug then
+  local osvpath = vim.fn.stdpath("data") .. "/lazy/one-small-step-for-vimkind"
+  vim.opt.rtp:append(osvpath)
+  require("osv").launch({ port = 8086, blocking = true })
+end
+
+require("config.lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
---
+++ lua/config/lsp.lua
@@ -0,0 +1,12 @@
+-- h: lsp-config
+
+-- enable lsp completion
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
+  callback = function(ev)
+    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
+  end
+})
+
+vim.lsp.enable("lua_ls")
