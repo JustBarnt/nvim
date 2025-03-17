1fa57f6fe73e4afe3c05aa84e62e9513156e5781
justbarnt <functnal@gmail.com>
2025-03-17 16:55:03 -0400

    revert: last commit

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -3,13 +3,25 @@ local cfg = require("module.lsp.lang.lua")
 
 ---@type vim.lsp.Config
 return {
-	cmd = { "lua-language-server" },
-	root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "selene.toml", "selene.yml", ".git" },
-	filetypes = { "lua" },
-	capabilities = utils.create_capabilities(),
-	on_exit = utils.on_exit,
-	on_error = utils.on_error,
-	on_init = function(client)
-		utils.on_init(client, cfg.settings)
-	end
+  cmd = { "lua-language-server" },
+  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "selene.toml", "selene.yml", ".git" },
+  filetypes = { "lua" },
+  ---@type lsp.ClientCapabilities
+  capabilities = utils.create_capabilities(),
+  --- Note: This does not work, they will have to server capabilities will have to manually overwritten
+  ---@type lsp.ServerCapabilities
+  server_capabilities = {
+    semanticTokensProvider = {
+      full = false,
+      legend = {
+        tokenTypes = {},
+        tokenModifiers = {},
+      },
+    },
+  },
+  on_exit = utils.on_exit,
+  on_error = utils.on_error,
+  on_init = function(client)
+    utils.on_init(client, cfg.settings)
+  end,
 }
