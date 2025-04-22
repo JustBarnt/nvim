ab63b7344d37cfa216d3f4284aaf0048dc1baf59
justbarnt <functnal@gmail.com>
2025-04-22 13:56:44 -0400

    updated CSS ruling

--- lsp/harper_ls.lua
+++ lsp/harper_ls.lua
@@ -1,34 +1,35 @@
-return {
-  cmd = { "harper-ls", "--stdio" },
-  root_markers = { ".git", "composer.json" },
-  filetypes = {
-    "c",
-    "cpp",
-    "gitcommit",
-    "go",
-    "html",
-    "javascript",
-    "lua",
-    "markdown",
-    "python",
-    "rust",
-    "toml",
-    "typescript",
-    "typescriptreact",
-    "cmake",
-    "php",
-    "ctp",
-    "c_sharp",
-  },
-  capabilities = Helpers.lsp.create_capabilities(),
-  on_init = function(client)
-    Helpers.lsp.on_init(client, {
-      harper_ls = {
-        linters = {
-          SentenceCapitalization = false,
-          SpellCheck = false,
-        },
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   cmd = { "harper-ls", "--stdio" },
+--   root_markers = { ".git", "composer.json" },
+--   filetypes = {
+--     "c",
+--     "cpp",
+--     "gitcommit",
+--     "go",
+--     "html",
+--     "javascript",
+--     "lua",
+--     "markdown",
+--     "python",
+--     "rust",
+--     "toml",
+--     "typescript",
+--     "typescriptreact",
+--     "cmake",
+--     "php",
+--     "ctp",
+--     "c_sharp",
+--   },
+--   capabilities = Helpers.lsp.create_capabilities(),
+--   on_init = function(client)
+--     Helpers.lsp.on_init(client, {
+--       harper_ls = {
+--         linters = {
+--           SentenceCapitalization = false,
+--           SpellCheck = false,
+--         },
+--       },
+--     })
+--   end,
+-- }
