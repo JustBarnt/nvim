1b5c4858cdc03ba36722056d58ac9b254ec882c6
justbarnt <functnal@gmail.com>
2025-04-24 09:05:34 -0400

    updated nord colors to show visual selection better

--- lua/plugins/ui/nordic.lua
+++ lua/plugins/ui/nordic.lua
@@ -1,6 +1,9 @@
 return {
   "justbarnt/nordic.nvim",
   opts = {
+    on_highlight = function(hl, p)
+      hl.Visual = { bg = p.gray1 }
+    end,
     swap_backgrounds = true,
     cursorline = {
       bold = true,
