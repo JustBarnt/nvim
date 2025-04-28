f5ce5d7ca57ced512e8f76bc7d3bd810c54eaedf
justbarnt <functnal@gmail.com>
2025-04-28 09:56:52 -0400

    lsp(css_modules): Made it require a workspace for the LSP to work

--- lsp/cssmodules_ls.lua
+++ lsp/cssmodules_ls.lua
@@ -2,4 +2,5 @@ return {
   cmd = { "cssmodules-language-server" },
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   root_markers = { "package.json" },
+  workspace_required = true,
 }
