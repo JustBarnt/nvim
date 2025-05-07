ee4c5499277eccea8557a0523553a306abe8bd17
justbarnt <functnal@gmail.com>
2025-05-07 14:06:21 -0400

    updates(mason): Updated mason.nvim to V2.0.0

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -19,13 +19,13 @@ return {
 
     vim.keymap.set("n", "<leader>cm", "<CMD>Mason<CR>", { desc = "Mason" })
 
-    local mr = require("mason-registry")
+    local mr = require "mason-registry"
     mr:on("package:install:success", function()
       vim.defer_fn(function()
-        require("lazy.core.handler.event").trigger({
+        require("lazy.core.handler.event").trigger {
           event = "FileType",
           buf = vim.api.nvim_get_current_buf(),
-        })
+        }
       end, 100)
     end)
 
@@ -35,21 +35,18 @@ return {
         local p = mr.get_package(tool)
         if not p:is_installed() then
           -- https://github.com/williamboman/mason-lspconfig.nvim/blob/1a31f824b9cd5bc6f342fc29e9a53b60d74af245/lua/mason-lspconfig/install.lua#L12
-          p:install():once(
-            "closed",
-            vim.schedule_wrap(function()
-              if p:is_installed() then
-                vim.notify(("[mason.nvim] %s was installed successfully"):format(p.name))
-              else
-                vim.notify(
-                  ("[mason.nvim] failed to install %s. Installation logs are available in :Mason and :MasonLog"):format(
-                    p.name
-                  ),
-                  vim.log.levels.ERROR
-                )
-              end
-            end)
-          )
+          p:install({}, function(success, results)
+            if success then
+              vim.notify(("[mason.nvim] %s was installed successfully"):format(p.name))
+            else
+              vim.notify(
+                ("[mason.nvim] failed to install %s. Installation logs are available in :Mason and :MasonLog"):format(
+                  p.name
+                ),
+                vim.log.levels.ERROR
+              )
+            end
+          end)
         end
       end
     end)
