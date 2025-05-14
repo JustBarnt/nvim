5a7bb1bc43186af4ffb8cb861391dc15d593584e
justbarnt <functnal@gmail.com>
2025-05-14 09:20:40 -0400

    updated undotree to handle its vim opt setting in init per lazy.nvim recommendations

--- lua/plugins/editor/undotree.lua
+++ lua/plugins/editor/undotree.lua
@@ -1,12 +1,10 @@
 return {
   "mbbill/undotree",
   event = "VeryLazy",
-  config = function()
-    if vim.g.loaded_undotree == 1 then
-      vim.g.undotree_WindowLayout = 2
-      if vim.fn.has("win32") == 1 then
-        vim.g.undotree_DiffCommand = "FC"
-      end
+  init = function()
+    vim.g.undotree_WindowLayout = 2
+    if vim.fn.has("win32") == 1 then
+      vim.g.undotree_DiffCommand = "FC"
     end
   end,
 }
