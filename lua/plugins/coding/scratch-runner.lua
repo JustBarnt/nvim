344f989d1244f4e9dfedbccafeff31fe2dd94e9f
justbarnt <functnal@gmail.com>
2025-05-14 14:18:08 -0400

    plugin to run scratch files for other languages

---
+++ lua/plugins/coding/scratch-runner.lua
@@ -0,0 +1,13 @@
+return {
+  "DestopLine/scratch-runner.nvim",
+  opts = {
+    sources = {
+      javascript = { "node" },
+      typescript = {
+        { "node" },
+        extension = "ts",
+      },
+      python = { "python" },
+    },
+  },
+}
