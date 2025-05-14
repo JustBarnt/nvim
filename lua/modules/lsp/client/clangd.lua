2606e5d768bc99d4024b3a046f1a8f773e2d5f45
justbarnt <functnal@gmail.com>
2025-05-14 13:44:40 -0400

    fixed spelling in assertion check

--- lua/modules/lsp/client/clangd.lua
+++ lua/modules/lsp/client/clangd.lua
@@ -3,7 +3,7 @@ local Clangd = {}
 ---@param client vim.lsp.Client
 ---@param keys LazyKeysSpec[]
 function Clangd.setup(client, keys)
-  assert(client.name == "Clangd", ("Unknown Client: **%s**"):format(client.name))
+  assert(client.name == "clangd", ("Unknown Client: **%s**"):format(client.name))
   vim.list_extend(
     keys,
     { { "<leader>ch", "<CMD>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" } }
