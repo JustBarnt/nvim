f341d87907b89d7734d12b2521b25bac1ad289bc
justbarnt <functnal@gmail.com>
2025-05-15 17:03:26 -0400

    checking out time-machine a diff/history plugin

---
+++ lua/plugins/editor/time-machine.lua
@@ -0,0 +1,15 @@
+return {
+  "y3owk1n/time-machine.nvim",
+  enabled = false,
+  version = "*",
+  init = function()
+    vim.opt.undofile = true
+    vim.opt.undodir = vim.fn.expand "~/.local/state/nvim-data/time-machine/"
+  end,
+  opts = function(_, opts)
+    local has_delta = vim.fn.executable("delta") == 1
+    return {
+      diff_tool = has_delta and "delta" or "native",,
+      external_diff_args = { "-s" },
+    } end,
+}
