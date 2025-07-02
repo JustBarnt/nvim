8f17ec11575cb51e8fdc62919858f8f06aa01dd0
justbarnt <functnal@gmail.com>
2025-07-02 10:17:47 -0400

    added to bigfile to hopefully making it easier to work inside larger XML files.

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -13,7 +13,23 @@ end
 return {
   "snacks.nvim",
   opts = {
-    bigfile = { enabled = true },
+    bigfile = {
+      notify = true,
+      size = 5 * 1024 * 1024,
+      line_length = 1000,
+      setup = function(ctx)
+        if vim.fn.exists(":NoMatchParen") ~= 0 then
+          vim.cmd[[NoMatchParen]]
+        end
+        Snacks.util.wo(0, { foldmethod = "indent", statuscolumn = "", conceallevel = 0 })
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(ctx.buf) then
+            vim.bo[ctx.buf].syntax = ctx.ft
+            Snacks.indent.disable()
+          end
+        end)
+      end,
+    },
     bufdelete = { enabled = true },
     dashboard = { enabled = true },
     indent = { enabled = true },
@@ -85,5 +101,6 @@ return {
     { "<leader>shi", function() Snacks.picker.highlights() end, desc = "Highlights"},
     { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
     { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+    { "<leader>sz", function() Snacks.picker.zoxide() end, desc = "Use Zoxide to find a directory" },
   },
 }
