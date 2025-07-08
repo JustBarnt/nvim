da683184c66868a8677aa46c582e3067b9ce170c
JustBarnt <functnal@gmail.com>
2025-07-07 22:35:18 -0400

    update tokyonight theme based on whether ghostty is the active terminal

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -1,5 +1,10 @@
+local is_transparent = vim.fn.getenv('TERM') == "xterm-ghostty"
+
 return {
   "folke/tokyonight.nvim",
   lazy = false,
-  priority = 10000
+  priority = 10000,
+  opts = {
+    transparent = is_transparent
+  }
 }
