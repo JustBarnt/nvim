ca626c0a1c30631181eeed407fc12b515db92447
JustBarnt <functnal@gmail.com>
2025-07-07 22:44:02 -0400

    Updated theme settings, delete treesitter context

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -1,10 +1,14 @@
-local is_transparent = vim.fn.getenv('TERM') == "xterm-ghostty"
+local is_transparent = vim.fn.getenv "TERM" == "xterm-ghostty"
 
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 10000,
   opts = {
-    transparent = is_transparent
-  }
+    transparent = is_transparent,
+    styles = {
+      sidebars = is_transparent and "transparent" or "dark",
+      floats = is_transparent and "transparent" or "dark",
+    },
+  },
 }
--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -21,7 +21,7 @@ return {
         -- stylua: ignore
         right_mouse_command = function(n) Snacks.bufdelete(n) end,
       diagnostics = "nvim_lsp",
-      always_show_bufferline = true,
+      always_show_bufferline = false,
       diagnostics_indicator = function(_, _, diag)
         local icons = Helpers.ui.icons.diagnostics
         local ret = (diag.error and icons.Error .. diag.error .. " " or "")
