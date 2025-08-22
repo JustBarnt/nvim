ad23ad9a6b7c9ccb5fb6864330c5a2e7d5b1773d
justbarnt <functnal@gmail.com>
2025-08-22 12:42:54 -0400

    changes to lemminx

--- lsp/lemminx.lua
+++ lsp/lemminx.lua
@@ -4,16 +4,41 @@ return {
   filetypes = { "xml", "xsd", "xsl", "xslt", "svg" },
   settings = {
     xml = {
-      completion = {
-        autoCloseTags = true,
+      fileAssociations = {
+        {
+          pattern = "**/*.xsl",
+          systemId = "https://raw.githubusercontent.com/JustBarnt/user-schemas/refs/heads/main/xml/xslt-1_0.xsd",
+          rootElement = "xsl:stylesheet",
+          namespace = "http://www.w3.org/1999/XSL/Transform",
+        },
+        {
+          pattern = "**/*.xslt",
+          systemId = "https://raw.githubusercontent.com/JustBarnt/user-schemas/refs/heads/main/xml/xslt-1_0.xsd",
+          rootElement = "xsl:stylesheet",
+          namespace = "http://www.w3.org/1999/XSL/Transform",
+        },
+        {
+          pattern = "**/*.xsl",
+          systemId = "https://raw.githubusercontent.com/JustBarnt/user-schemas/refs/heads/main/xml/xslt-1_0.xsd",
+          rootElement = "xsl:transform",
+          namespace = "http://www.w3.org/1999/XSL/Transform",
+        },
+        {
+          pattern = "**/*.xslt",
+          systemId = "https://raw.githubusercontent.com/JustBarnt/user-schemas/refs/heads/main/xml/xslt-1_0.xsd",
+          rootElement = "xsl:transfom",
+          namespace = "http://www.w3.org/1999/XSL/Transform",
+        },
       },
-      format = {
-        enabled = true,
-      },
-      foldings = {
-        includeClosingTagInFold = true,
-      },
+      -- catalogs = {
+      --     "D:/Personal/Github/user-schemas/xml/xslt-1_0-catalog.xml", -- XSLT 1.0 Schema
+      -- },
+      validation = { noGrammar = "hint" },
+      completion = { autoCloseTags = true },
+      caching = { useCache = true },
+      format = { enabled = true },
+      foldings = { includeClosingTagInFold = true },
     },
   },
-  capabilities = Helpers.lsp.create_capabilities({ textDocument = { formatting = { dynamicRegistration = false } } }),
+  capabilities = Helpers.lsp.create_capabilities { textDocument = { formatting = { dynamicRegistration = false } } },
 }
