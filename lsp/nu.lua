2bfc828c3dfaf7ebb14be5c3885c7f1d259503c2
justbarnt <functnal@gmail.com>
2025-08-29 10:52:45 -0400

    updates to nu-lsp

--- lsp/nu.lua
+++ lsp/nu.lua
@@ -2,6 +2,10 @@
 return {
   cmd = { "nu", "--lsp" },
   root_markers = { ".git" },
+  root_dir = function(fname)
+    return vim.fs.dirname(fname)
+  end,
+  flags = { debounce_text_changes = 1000 },
   filetypes = { "nu" },
   workspace_required = false,
   capabilities = Helpers.lsp.create_capabilities(),
