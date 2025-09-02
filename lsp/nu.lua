840569badaa108c5a2e6eb0c7aa0c28f75ee1900
justbarnt <functnal@gmail.com>
2025-09-02 12:12:22 -0400

    removed noice.nvim

--- lsp/nu.lua
+++ lsp/nu.lua
@@ -2,9 +2,6 @@
 return {
   cmd = { "nu", "--lsp" },
   root_markers = { ".git" },
-  root_dir = function(fname)
-    return vim.fs.dirname(fname)
-  end,
   flags = { debounce_text_changes = 1000 },
   filetypes = { "nu" },
   workspace_required = false,
