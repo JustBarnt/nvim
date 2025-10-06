9f05387def8da0b735153ca3841d7b0ada645913
justbarnt <functnal@gmail.com>
2025-10-06 14:47:38 -0400

    tinkering with cinpoptions

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,4 +1,4 @@
-vim.opt_local.cindent = true
+vim.opt_local.cindent = false
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
-vim.opt_local.cinoptions = "g2,N-s,:0,l1"
+vim.opt_local.cinoptions = "g2,h2"
