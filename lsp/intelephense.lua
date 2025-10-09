b50656a18808022689bf40b9a79b5b42a01d6c8a
justbarnt <functnal@gmail.com>
2025-10-09 10:00:43 -0400

    lsp: updated intelephense settings

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,17 +1,16 @@
+---@type vim.lsp.ClientConfig
 return {
   cmd = { "intelephense", "--stdio" },
   root_markers = { ".git", "composer.json" },
   filetypes = { "php", "ctp" },
   capabilities = Helpers.lsp.create_capabilities(),
-  on_init = function(client)
-    Helpers.lsp.on_init(client, {
-      intelephense = {
-        environment = {
-          includePaths = {
-            "C:\\PHP\\includes",
-          },
+  settings = {
+    intelephense = {
+      environment = {
+        includePaths = {
+          "C:\\PHP\\includes",
         },
       },
-    })
-  end,
+    },
+  },
 }
