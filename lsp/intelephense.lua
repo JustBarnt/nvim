46eeff99fca10bc3f569eae666bbe5d8c9b01795
justbarnt <functnal@gmail.com>
2025-10-24 14:29:27 -0400

    updates(lsp): intelephense

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,18 +1,9 @@
 ---@type vim.lsp.ClientConfig
 return {
   cmd = { "intelephense", "--stdio" },
-  root_markers = { ".git", "composer.json" },
+  root_markers = { ".git", "composer.json", ".svn" },
   filetypes = { "php", "ctp" },
   capabilities = { },
-  settings = {
-    intelephense = {
-      environment = {
-        includePaths = {
-          "C:\\PHP\\includes",
-        },
-      },
-    },
-  },
   on_attach = function(client, bufnr)
     -- NOTE: USE TREESITTER INDENT FOR PHP. Intelephense's `GetPhpIndent()`
     --       SUCKS
