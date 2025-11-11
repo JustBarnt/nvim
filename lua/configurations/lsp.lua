85a234f1441c1a6b1e2b8b6e63a58d81d50c45a8
justbarnt <functnal@gmail.com>
2025-11-11 16:54:35 -0500

    wip: lsp setup

--- lua/configurations/lsp.lua
+++ lua/configurations/lsp.lua
@@ -1,12 +1,44 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
 ---@class config.lsp
+---@field debuggers string[]
+---@field formatters string[]
+---@field language_servers string[]
+---@field linters string[]
 local M = {}
 
+--- Creates Client Capabilities
+---@return lsp.ClientCapabilities
+local function create_capabilities()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.foldingRange = {
+    dynamicRegistration = true,
+    lineFoldingOnly = true
+  }
+
+  capabilities.textDocument.semanticTokens.multilineTokenSupport = true
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  local ok, blink = pcall(require, "blink.cmp")
+  if ok then
+    capabilities = blink.get_lsp_capabilities(capabilities)
+  end
+
+  return capabilities
+end
+
+local function lsp_attach()
+  local lsp_group = augroup("barnt/lsp_attach", { clear = true })
+  autocmd("LspAttach", {
+    group = lsp_group,
+  })
+end
+
 --stylua: ignore
----@type string[]
 M.debuggers = {}
 
 --stylua: ignore
----@type string[]
 M.formatters = {
   "clang-format", "gofumpt", "goimports",
   "gomodifytags", "shfmt", "stylua",
@@ -14,7 +46,6 @@ M.formatters = {
 }
 
 --stylua: ignore
----@type string[]
 M.language_servers = {
   "clangd", "cmake-language-server", "css-lsp", "css-variables-language-server",
   "cssmodules-language-server", "emmet-ls", "gopls", "html-lsp",
@@ -25,7 +56,21 @@ M.language_servers = {
 }
 
 --stylua: ignore
----@type string[]
 M.linters = { "cmakelint", "shellcheck" }
+
+---Sets up various LSP Capabilities
+---@param capabilities any
+M.setup = function(capabilities)
+  vim.lsp.config("*", {
+    capabilities = create_capabilities()
+  })
+
+  -- Remove default keybinds
+  for _, bind in ipairs({"grn", "gra", "gri", "grr", "grt"}) do
+      pcall(vim.keymap.del, "n", bind)
+  end
+
+  lsp_attach()
+end
 
 return M
