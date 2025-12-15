f1bf8feb2f85ea115497a5536aff0e342273fcff
justbarnt <functnal@gmail.com>
2025-12-15 15:11:49 -0500

    update: mini icons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,28 @@ return {
   {
     "nvim-mini/mini.icons",
     config = function()
-      require("mini.icons").setup()
+      require("mini.icons").setup({
+        style = "glyph",
+        file = {
+          ["artisan"] = { glyph = "", hl = "MiniIconsRed" },
+          [".axaml"] = { glyph = "󰙳", hl = "MiniIconsGreen" },
+          [".xaml"] = { glyph = "󰙳", hl = "MiniIconsGreen" },
+          [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+          [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+          [".node-version"] = { glyph = " ", hl = "MiniIconsGreen" },
+          [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+          ["eslint.config.js"] = { glyph = "󰱺 ", hl = "MiniIconsYellow" },
+          ["package.json"] = { glyph = " ", hl = "MiniIconsGreen" },
+          ["tsconfig.json"] = { glyph = " ", hl = "MiniIconsAzure" },
+          ["tsconfig.build.json"] = { glyph = " ", hl = "MiniIconsAzure" },
+          ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+          ["composer.lock"] = { glyph = "", hl = "MiniIconsPurple"},
+        },
+        filetype = {
+          axaml = { glyph = "󰙳 ", hl = "MiniIconsGreen" },
+          xaml = { glyph = "󰙳 ", hl = "MiniIconsGreen" },
+        },
+      })
       MiniIcons.mock_nvim_web_devicons()
     end,
   },
