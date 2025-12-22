ba4a3983ef0731ebeaafe706c626cd99543bfe41
justbarnt <functnal@gmail.com>
2025-12-22 09:49:10 -0500

    feat(roslyn): Added roslyn.nvim

---
+++ lua/plugins/lsp/roslyn.lua
@@ -0,0 +1,11 @@
+--- @module "roslyn"
+return {
+  "seblyng/roslyn.nvim",
+  ft = "cs",
+  ---@type RoslynNvimConfig
+  opts = {
+    filewatching = "auto",
+    broad_search = false,
+    lock_target = true,
+  }
+}
