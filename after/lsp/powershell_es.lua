b43a1d06807a7f5b963b90b697f185930eff0d5f
justbarnt <functnal@gmail.com>
2026-02-02 10:44:48 -0500

    lsp(powershell_es): disable semanticToken support. Its not good

--- after/lsp/powershell_es.lua
+++ after/lsp/powershell_es.lua
@@ -1,4 +1,9 @@
 ---@type vim.lsp.Config
 return {
   bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+  capabilities = {
+    textDocument = {
+       semanticTokens = nil
+    }
+  }
 }
