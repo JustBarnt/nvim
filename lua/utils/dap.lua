d2862784bbbdc4b23742179987ce66c4c7634915
justbarnt <functnal@gmail.com>
2026-05-27 14:01:56 -0400

    dap adapters

--- lua/utils/dap.lua
+++ lua/utils/dap.lua
@@ -18,6 +18,26 @@ function M.init()
       port = 9003,
     },
   }
+
+  dap.adapters.codelldb = {
+    type = "executable",
+    command = vim.fn.stdpath("data") .. '/mason/bin/codelldb.cmd',
+    detached = false
+  }
+
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. "/", 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = false
+    }
+  }
+  
 end
 
 return M
