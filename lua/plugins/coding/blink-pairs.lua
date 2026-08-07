037e0b8b496972fd27efbe6c8a5107aabb391a33
justbarnt <functnal@gmail.com>
2026-08-07 12:59:59 -0400

    updates

--- lua/plugins/coding/blink-pairs.lua
+++ lua/plugins/coding/blink-pairs.lua
@@ -3,7 +3,9 @@ return {
     'saghen/blink.pairs',
     version = "v0.6.0",
     dependencies = { "saghen/blink.lib" },
-    build = require("blink-pairs").build():pwait(60000),
+    build = function()
+      require("blink-pairs").build():pwait(60000)
+    end,
     opts = {}
   }
 }
